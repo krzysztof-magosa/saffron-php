7464c7fad24940f6688b4fd7039cdba5f924307e
Krzysztof Magosa <krzysztof@magosa.pl>
2014-10-03 23:53:30 +0200

    Some tests

---
+++ tests/RouterTest.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ * Copyright 2014 Krzysztof Magosa
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+class RouterTest extends PHPUnit_Framework_TestCase
+{
+    public function provider()
+    {
+        return [
+            ['/product', 'POST', 'create'],
+            ['/product/100', 'POST', 'update'],
+            ['/product/200', 'GET', 'get'],
+            ['/product/300', 'DELETE', 'delete'],
+        ];
+    }
+
+    /**
+     * @dataProvider provider
+     */
+    public function testDispatch($uri, $method, $expected)
+    {
+        $router = new \KM\Saffron\Router();
+        $router
+            ->append(
+                [
+                    'name' => 'create',
+                    'uri' => '/product',
+                    'method' => 'POST',
+                    'target' => ['ProductController', 'createAction'],
+                    'default' => ['routeName' => 'create'],
+                ]
+            )
+            ->append(
+                [
+                    'name' => 'update',
+                    'uri' => '/product/{id}',
+                    'method' => 'POST',
+                    'target' => ['ProductController', 'updateAction'],
+                    'default' => ['routeName' => 'update'],
+                ]
+            )
+            ->append(
+                [
+                    'name' => 'get',
+                    'uri' => '/product/{id}',
+                    'method' => 'GET',
+                    'target' => ['ProductController', 'getAction'],
+                    'default' => ['routeName' => 'get'],
+                ]
+            )
+            ->append(
+                [
+                    'name' => 'delete',
+                    'uri' => '/product/{id}',
+                    'method' => 'DELETE',
+                    'target' => ['ProductController', 'deleteAction'],
+                    'default' => ['routeName' => 'delete'],
+                ]
+            );
+
+        $request = new KM\Saffron\Request();
+        $request
+            ->setUri($uri)
+            ->setMethod($method);
+
+        $route = $router->dispatch($request);
+        
+        $this->assertEquals($expected, $route->getParam('routeName'));
+    }
+
+    public function testOptionalParam()
+    {
+        $router = new \KM\Saffron\Router();
+        $router
+            ->append(
+                [
+                    'name' => 'get',
+                    'uri' => '/product/{id}',
+                    'method' => 'GET',
+                    'target' => ['ProductController', 'getAction'],
+                    'default' => ['id' => 12345],
+                ]
+            );
+
+        $request = new KM\Saffron\Request();
+        $request
+            ->setUri('/product')
+            ->setMethod('GET');
+
+        $route = $router->dispatch($request);
+        
+        $this->assertEquals(12345, $route->getParam('id'));
+    }
+}
