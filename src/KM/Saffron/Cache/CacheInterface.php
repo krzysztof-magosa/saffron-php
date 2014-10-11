6d6fe74f22351d9dc2a1d3589f7f98f0f0f8328a
Krzysztof Magosa <krzysztof@magosa.pl>
2014-10-11 17:29:12 +0200

    Remove Factory and related classes

--- src/KM/Saffron/Cache/CacheInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace KM\Saffron\Cache;
-
-interface CacheInterface
-{
-    public function get($key);
-    public function set($key, $value, $ttl);
-    public function isSupported();
-}
