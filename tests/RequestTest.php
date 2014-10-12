2c15f84ee00bdab2f1eb34ee30f4911fdddf00a0
Krzysztof Magosa <krzysztof@magosa.pl>
2014-10-12 14:04:18 +0200

    Request tests

---
+++ tests/RequestTest.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Copyright 2014 Krzysztof Magosa
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+use \KM\Saffron\Request;
+
+class RequestTest extends PHPUnit_Framework_TestCase
+{
+    public function testDomain()
+    {
+        $request = new Request();
+        $request->setDomain('example.com');
+        $this->assertEquals('example.com', $request->getDomain());
+    }
+
+    public function testUri()
+    {
+        $request = new Request();
+        $request->setUri('/test/uri');
+        $this->assertEquals('/test/uri', $request->getUri());
+    }
+
+    public function testMethod()
+    {
+        $request = new Request();
+        $request->setMethod('GET');
+        $this->assertEquals('GET', $request->getMethod());
+    }
+
+    public function testGlobals()
+    {
+        $_SERVER['REQUEST_URI'] = '/another/uri';
+        $_SERVER['REQUEST_METHOD'] = 'POST';
+        $_SERVER['HTTP_HOST'] = 'example.com';
+  
+        $request = Request::createFromGlobals();
+        
+        $this->assertEquals('/another/uri', $request->getUri());
+        $this->assertEquals('POST', $request->getMethod());
+        $this->assertEquals('example.com', $request->getDomain());
+    }
+}
