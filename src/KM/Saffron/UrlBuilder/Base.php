b2f0b6f023c03b38be3f36f78bcfdf4b1ada978d
Krzysztof Magosa <krzysztof.magosa@performgroup.com>
2014-10-16 09:57:25 +0200

    UrlBuilder

---
+++ src/KM/Saffron/UrlBuilder/Base.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Copyright 2014 Krzysztof Magosa
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+namespace KM\Saffron\UrlBuilder;
+
+use KM\Saffron\Exception\NoSuchRoute;
+
+abstract class Base
+{
+    protected $routes;
+
+    protected function hasRoute($name)
+    {
+        return isset($this->routes[$name]);
+    }
+
+    protected function getRoute($name)
+    {
+        if (!$this->hasRoute($name)) {
+            throw new NoSuchRoute("There is no route $name.");
+        }
+
+        return $this->routes[$name];
+    }
+
+    public function assemble($name, array $parameters = [])
+    {
+        $route = $this->getRoute($name);
+
+        $values = array_replace(
+            $route['defaults'],
+            $parameters
+        );
+
+        $uri = $route['uri'];
+        foreach ($values as $name => $value) {
+            $uri = str_replace('{'.$name.'}', $value, $uri);
+        }
+
+        return $uri;
+    }
+}
