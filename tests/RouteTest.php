2ceea0378476fdf3e170ca4eac5bf4add1cb50e2
Krzysztof Magosa <krzysztof@magosa.pl>
2014-10-19 00:20:22 +0200

    Route test

--- tests/RouteTest.php
+++ tests/RouteTest.php
@@ -29,4 +29,11 @@ class RouteTest extends PHPUnit_Framework_TestCase
         $route->setRequirements(['id' => '\d+']);
         $this->assertEquals(['id' => '\d+'], $route->getRequirements());
     }
+
+    public function testDomainPlaceholders()
+    {
+        $route = new Route('name1');
+        $route->setDomain('{prefix}.example.{tld}');
+        $this->assertEquals(['prefix', 'tld'], $route->getDomainPlaceholders());
+    }
 }
